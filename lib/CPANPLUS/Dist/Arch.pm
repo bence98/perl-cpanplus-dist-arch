bence98/perl-cpanplus-dist-arch

commit 7930826f40ac05504dcd73362403c29d853bc171

Added the package template (get/set_)pkgbuild_templ methods and tests.

lib/CPANPLUS/Dist/Arch.pm
@@ -47,7 +47,6 @@ END_MSG
 my $PACMAN_FINDOWN     = qr/\A[^ ]+ is owned by ([\w-]+) ([\w.-]+)/;
 my $PACMAN_FINDOWN_ERR = qr/\Aerror:/;
 
-
 # Override a package's name to conform to packaging guidelines.
 # Copied entries from CPANPLUS::Dist::Pacman and alot more
 # from searching for packages with perl in their name in
@@ -74,21 +73,6 @@ SDL_Perl       = sdl_perl
 shorewall-perl = shorewall-perl
 
 END_OVERRIDES
-
-=for Mini-Template Format
-    The template format is very simple, to insert a template variable
-    use [% foo %].  This will insert the value of the variable
-    named 'foo'.
- 
-    The print_template() sub will throw: 'Template variable ... was
-    not provided' if the variable was not defined.
- 
-    [% IF var_name %] ... [% FI %] will remove the ... stuff if the
-    variable named var_name is not set to a true value.
- 
-    See the _process_template method below.
-
-=cut
 
 # Crude template for our PKGBUILD script
 my $PKGBUILD_TEMPL = <<'END_TEMPL';
@@ -218,7 +202,7 @@ sub init
 
     $self->status->mk_accessors( qw{ pkgname  pkgver  pkgbase pkgdesc
                                      pkgurl   pkgsize pkgarch
-                                     builddir destdir } );
+                                     builddir destdir pkgbuild_templ  } );
     return 1;
 }
 
@@ -550,6 +534,20 @@ sub get_pkgvars_ref
     return { $self->get_pkgvars };
 }
 
+sub set_pkgbuild_templ
+{
+    my ($self, $template) = @_;
+
+    return $self->status->pkgbuild_templ( $template );
+}
+
+sub get_pkgbuild_templ
+{
+    my ($self) = @_;
+
+    return $self->status->pkgbuild_templ() || $PKGBUILD_TEMPL;
+}
+
 sub get_pkgbuild
 {
     croak 'Invalid arguments to get_pkgbuild' if ( @_ < 1 );
@@ -580,8 +578,9 @@ sub get_pkgbuild
           $dist_type eq 'CPANPLUS::Dist::Build' ? (0, 1) :
           die "unknown Perl module installer type: '$dist_type'" );
 
-    return scalar $self->_process_template( $PKGBUILD_TEMPL,
-                                            $templ_vars );
+    my $templ_text = $status->pkgbuild_templ || $PKGBUILD_TEMPL;
+
+    return scalar $self->_process_template( $templ_text, $templ_vars );
 }
 
 sub create_pkgbuild

lib/CPANPLUS/Dist/Arch.pod
@@ -239,6 +239,39 @@ included in the examples directory for an example.
             Invalid directoy passed to create_pkgbuild: ...
   Returns : Nothing.
 
+=head2 get_pkgbuild_templ
+
+  Usage   : my $tt_text = $self->get_pkgbuild_templ();
+  Purpose : Mostly just to compliment L</set_pkgbuild_templ>.
+  Returns : The template used internally by L</get_pkgbuild>
+
+See L</PKGBUILD TEMPLATE>.
+
+=head2 set_pkgbuild_templ
+
+  Usage   : my $new_tt_text = $self->set_pkgbuild_templ( $templ_text );
+  Purpose : Create PKGBUILDs based on a template.
+  Params  : $templ_text - New template text to use for creating the PKGBUILD.
+  Returns : The new template text.
+            
+See L</PKGBUILD TEMPLATE>.
+
+=head1 PKGBUILD TEMPLATE
+
+The template format is an extremely simplified ripoff of
+L<Template::Toolkit> syntax.  It only implements variable
+interpolation and the C<IF> directive.
+
+To insert a template variable use the syntax C<[% foo %]>.  This will
+insert the value of the variable named 'foo'.  The C<get_pkgbuild()>
+method will croak with the message I<Template variable ... was not
+provided> if the variable was not defined.  C<[% IF var_name %]>
+... C<[% FI %]> will remove what is in-between the C<IF> and C<FI>
+tags if the variable named C<var_name> is not set to a true value.
+
+See L</get_pkgvars> for a list of the variables that can be used
+inside the template.
+
 =head1 LIMITATIONS
 
 There are some limitations in the way CPANPLUS and pacman works

t/03-pkgbuild.t
@@ -1,0 +1,29 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use Test::More tests => 4;
+
+BEGIN {
+    # The ::Test class is under out test directory.
+    use lib qw(t/lib);
+    use_ok( 'CPANPLUS::Dist::Arch::Test' );
+}
+
+my $cda_obj = CPANPLUS::Dist::Arch::Test->new( name    => 'Fake-Package',
+                                               version => '31337' );
+
+## PKGBUILD TEMPLATES
+##############################################################################
+
+ok( $cda_obj->get_pkgbuild_templ() );
+
+my $templ = << 'END_TEMPL';
+[% pkgname %]
+[% pkgver %]
+END_TEMPL
+
+$cda_obj->set_pkgbuild_templ( $templ );
+is( $cda_obj->get_pkgbuild_templ, $templ );
+
+is( $cda_obj->get_pkgbuild(), "perl-fake-package\n31337\n" );

commit 63f146afd7ee996bcdaed8613391dfef4dcd4826

Actually use `get_pkgbuild_templ`
Fixes: 7930826f40ac ("Added the package template (get/set_)pkgbuild_templ
methods and tests.")

lib/CPANPLUS/Dist/Arch.pm
@@ -790,7 +790,7 @@ sub get_pkgbuild
           $dist_type eq 'CPANPLUS::Dist::Build' ? (0, 1) :
           die "unknown Perl module installer type: '$dist_type'" );
 
-    my $templ_text = $status->pkgbuild_templ || $PKGBUILD_TEMPL;
+    my $templ_text = $self->get_pkgbuild_templ;
 
     return scalar $self->_process_template( $templ_text, $templ_vars );
 }